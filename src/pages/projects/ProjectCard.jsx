4ba2a7b1664b0291b06824acb4a5e07e0e7b8435
Kelsey Thang <thang.kelsey@gmail.com>
2023-08-20 11:01:27 -0400

    update projects layout

--- src/pages/projects/ProjectCard.jsx
+++ src/pages/projects/ProjectCard.jsx
@@ -1,56 +1,56 @@
-import React from 'react'
-import { Box, Stack, useMediaQuery } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import { Box, Card, CardActions, CardContent, CardMedia, Stack, Typography, useMediaQuery } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import YouTubeIcon from '@mui/icons-material/YouTube'
 import LaunchIcon from '@mui/icons-material/Launch';
+import { Slide } from 'react-slideshow-image';
 import SkillSection from './SkillSection';
 import ProjectButton from './ProjectButton';
+import 'react-slideshow-image/dist/styles.css';
 
-const ProjectCard = ({ image, title, description, skills, points, gitLink, demoLink, liveLink }) => {
+const ProjectCard = ({ images, title, description, skills, points, gitLink, demoLink, liveLink }) => {
   const isMobile = useMediaQuery(theme => theme.breakpoints.down('md'));
 
   return (
-    <Card sx={{ width: '100%', background: 'linear-gradient(45deg, #29b6f6 30%, #FF8E53 90%)', mb: 2 }}>
-      <Stack direction={{ xs: 'column', sm: 'row' }}>
-        {isMobile ? (
-          <CardMedia
-            sx={{ height: 140 }}
-            image={image}
-            title='project'
-          />
-        ) : (
-          <img src={image} alt='project' style={{ width: '40%' }} />
-        )}
-        <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='div' sx={{ color: 'white', fontWeight: 'bold', mb: 2 }}>
-              {title}
-            </Typography>
-            <Stack direction='row' sx={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
-              {skills.map(skill => (
-                <SkillSection title={skill} key={skill} />
-              ))}
-            </Stack>
-            <Typography variant='body1' color='secondary' my={2}> 
-              {description}
-            </Typography>
-            {points.map(point => (
-              <Typography variant='body2' color='secondary' key={point}><CheckIcon fontSize='small' /> {point}</Typography>
+    <Card sx={{ width: '100%', height: '100%', background: 'linear-gradient(45deg, #29b6f6 30%, #FF8E53 90%)' }}>
+      {!isMobile ? (
+        <Slide autoplay={false}>
+            {images.map(image => (
+              <div key={image} className='each-slide-effect'>
+                <div style={{ 'backgroundImage': `url(${image})` }}></div>
+              </div>
             ))}
-          </CardContent>
-          <CardActions sx={{ ml: 1, my: 2 }}>
-            <ProjectButton icon={<GitHubIcon />} link={gitLink} title='GitHub' />
-            {demoLink && <ProjectButton icon={<YouTubeIcon />} link={demoLink} title='Demo' />}
-            {liveLink && <ProjectButton icon={<LaunchIcon />} link={liveLink} title='Live App' />}
-          </CardActions>
-        </Box>
-      </Stack>
+          </Slide>
+      ) : (
+        <CardMedia
+          sx={{ height: 150 }}
+          image={images[0]}
+          title='project'
+        /> 
+      )}
+      <Box sx={{ display: 'flex', flexDirection: 'column'}}>
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div' sx={{ color: 'white', fontWeight: 'bold', mb: 2 }}>
+            {title}
+          </Typography>
+          <Stack direction='row' sx={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
+            {skills.map(skill => (
+              <SkillSection title={skill} key={skill} />
+            ))}
+          </Stack>
+          <Typography variant='body1' color='secondary' my={2}> 
+            {description}
+          </Typography>
+          {points.map(point => (
+            <Typography variant='body2' color='secondary' key={point}><CheckIcon fontSize='small' /> {point}</Typography>
+          ))}
+        </CardContent>
+        <CardActions sx={{ ml: 1, my: 2 }}>
+          <ProjectButton icon={<GitHubIcon />} link={gitLink} title='GitHub' />
+          {demoLink && <ProjectButton icon={<YouTubeIcon />} link={demoLink} title='Demo' />}
+          {liveLink && <ProjectButton icon={<LaunchIcon />} link={liveLink} title='Live App' />}
+        </CardActions>
+      </Box>
     </Card>
   )
 }
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,47 +1,64 @@
 import React from 'react'
 import { Box, Container, Stack, Typography, useMediaQuery } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
   return (
     <Container maxWidth='lg'>
       <Typography variant='h4' fontFamily='Anton' color='secondary' mb={2}>Projects</Typography>
-      <ProjectCard 
-        image='https://imgur.com/Fci4tvP.png'
-        title='FFTCG Collector'
-        description='Trading card application for the Final Fantasy Trading Card Game.'
-        skills={['JavaScript', 'React', 'Ruby', 'Ruby on Rails', 'MUI', 'Active Storage', 'Deployed']}
-        points={[
-          'Users can create and login to an account via authentication', 
-          'Users can create and delete cards from their collection',
-          'Admins can create, update, and delete cards from the database',
-          'Admins can create, update, and delete expansions from the database'
-        ]}
-        gitLink='https://github.com/kelseythang/fftcg-collector'
-        liveLink='https://fftcg-collector.onrender.com/'
-      />
-      <ProjectCard 
-        image='https://imgur.com/VpQnVPI.png'
-        title='Discover Flats'
-        description='Apartment search application. '
-        skills={['JavaScript', 'React', 'Ruby', 'Ruby on Rails', 'MUI']}
-        points={[
-          'Users can create and login to an account via authentication',
-          'Users can create reviews on a listing'
-        ]}
-        gitLink='https://github.com/kelseythang/discover-flats'
-      />
-      <ProjectCard 
-        image='https://imgur.com/LQSX48a.png'
-        title='Library Manager'
-        description='Inventory and user management application for libraries.'
-        skills={['JavaScript', 'React', 'Ruby', 'MUI', 'Sinatra']}
-        points={[
-          'Users can create and delete library members',
-          'Users can create and delete book checkouts'
-        ]}
-        gitLink='https://github.com/kelseythang/library-manager-frontend'
-      />
+      <Grid container spacing={2}>
+        <Grid xs={12} md={6}>
+          <ProjectCard 
+            images={[
+              'https://imgur.com/iBKoUVz.png', 
+              'https://imgur.com/emFs4Nm.png',
+              'https://imgur.com/cHvPjMQ.png',
+              'https://imgur.com/Fci4tvP.png'
+            ]}
+            title='FFTCG Collector'
+            description='Trading card application for the Final Fantasy Trading Card Game.'
+            skills={['JavaScript', 'React', 'Ruby', 'Ruby on Rails', 'MUI', 'Active Storage', 'Deployed']}
+            points={[
+              'Users can create and login to an account via authentication', 
+              'Users can create and delete cards from their collection',
+              'Admins can create, update, and delete cards from the database',
+              'Admins can create, update, and delete expansions from the database'
+            ]}
+            gitLink='https://github.com/kelseythang/fftcg-collector'
+            liveLink='https://fftcg-collector.onrender.com/'
+          />
+        </Grid>
+        <Grid xs={12} md={6}>
+          <ProjectCard 
+            images={[
+              'https://imgur.com/VpQnVPI.png',
+              'https://imgur.com/g5CxDfP.png'
+            ]}
+            title='Discover Flats'
+            description='Apartment search application. '
+            skills={['JavaScript', 'React', 'Ruby', 'Ruby on Rails', 'MUI']}
+            points={[
+              'Users can create and login to an account via authentication',
+              'Users can create reviews on a listing'
+            ]}
+            gitLink='https://github.com/kelseythang/discover-flats'
+          />
+        </Grid>
+        <Grid xs={12} md={6}>
+          <ProjectCard 
+            images={['https://imgur.com/LQSX48a.png']}
+            title='Library Manager'
+            description='Inventory and user management application for libraries.'
+            skills={['JavaScript', 'React', 'Ruby', 'MUI', 'Sinatra']}
+            points={[
+              'Users can create and delete library members',
+              'Users can create and delete book checkouts'
+            ]}
+            gitLink='https://github.com/kelseythang/library-manager-frontend'
+          />
+        </Grid>
+      </Grid>
     </Container>
   )
 }
